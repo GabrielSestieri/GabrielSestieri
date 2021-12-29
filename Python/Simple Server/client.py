cf652b69ae5a80fef1d58b5779cf2425c0c8c8a6
GabrielSestieri <63177325+GabrielSestieri@users.noreply.github.com>
2021-12-29 15:13:33 -0500

    goodnight

--- Python/Simple Server/client.py
+++ Python/Simple Server/client.py
@@ -1,27 +1,50 @@
 import socket
+import time
+import threading
 
-HEADER = 64
-PORT = 5050
+PORT = 5051
 SERVER = "192.168.1.31"
-FORMAT = "utf-8"
-DISCONNECT_MESSAGE = "!DISCONNECT"
 ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+DISCONNECT = "!DISCONNECT"
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+def connect():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+    return client
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b" " * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-while True:
-    print(client.recv(2048).decode(FORMAT))
-    send(input("Send a message: "))
+def send(client, msg):
+    client.send(msg.encode())
 
+def start():
+    answer = input("Connect to the server? (y/n) ")
+    if answer.lower() != "y":
+        return
 
+    connection = connect()
+    username_status = False
+    while not username_status:
+        username = input("Enter username: ")
+        fullname = input("Enter full name: ")
+        if username != '':
+            send(connection, username+"|"+fullname)
+            username_status = True
+        else:
+            print("Username is required")
+            username_status = False
+
+    while True:
+        msg = (input("Send a message: (q for quit): "))
+        if msg == "q":
+            break
+        send(connection, msg)
+    thread = threading.Thread(target=connect)
+    thread.start()
+    send(connection, DISCONNECT)
+    time.sleep(1)
+    print("Disconnected")
+
+start()
 
 
 
--- Python/Simple Server/server.py
+++ Python/Simple Server/server.py
@@ -1,40 +1,78 @@
 import socket
 import threading
-from datetime import datetime
+from datetime import *
 
-HEADER = 64
-PORT = 5050
+
+PORT = 5051
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = "utf-8"
 DISCONNECT = "!DISCONNECT"
 
+ACTIVE_CLIENTS = {}
+USERNAME_STATUS = {}
+username = ""
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind(ADDR)
+
+clients = set()
+clients_lock = threading.Lock()
 
 def handle_client(connection, address):
     print(f"[NEW CONNECTION] {address} connected.")
-    
-    
-    connected = True 
-    while connected:
-        msg_length = connection.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = connection.recv(msg_length).decode(FORMAT)
+    try:
+        connected = True
+        while connected:
+            #GETS USERNAME
+            with clients_lock:
+                for c in clients:
+                    ip = c.getpeername()[0]
+                    if ip == address[0]:
+                        if USERNAME_STATUS[ip] == False:
+                            response = connection.recv(2048).decode('utf-8')
+                            username = response.split("|")[0]
+                            fullname = response.split("|")[1]
+                            if username != '':
+                                ACTIVE_CLIENTS[address[0]] = [username, fullname]
+                                print(ACTIVE_CLIENTS)
+                                USERNAME_STATUS[ip] = True
+                                break
+                            else:
+                                print("Username is empty")
+                        else:
+                            break
+            #GETS MESSAGES
+            msg = connection.recv(2048).decode(FORMAT)
+            if not msg:
+                break
             if msg == DISCONNECT:
                 connected = False
-            print(f"[{address}] {msg}")
-            now = datetime.now().strftime("%H:%M:%S")
-            connection.send(f"Message received at {now}".encode(FORMAT))
-            
-    connection.close()
+            with clients_lock:
+                for c in clients:
+                    ip = c.getpeername()[0]
+                    user = ACTIVE_CLIENTS.get(str(ip))
+                    time = datetime.now().strftime("%H:%M")
+                    if msg == "show me":
+                        print(ACTIVE_CLIENTS[ip])
+                    if ip == address[0]:
+                        print(f"[{time}] {user[0]}: {msg}")
+            #RANDOM COMMENT                     
+    finally:
+        with clients_lock:
+            clients.remove(connection)
+        connection.close()
 
 def start_server():
     server.listen()
     print(f"[LISTENING] server is listening on {SERVER}")
     while True:
         connection, address = server.accept()
+        with clients_lock:
+            clients.add(connection)
+            ACTIVE_CLIENTS[address[0]] = ""
+            USERNAME_STATUS[address[0]] = False
         thread = threading.Thread(target=handle_client, args=(connection, address))
         thread.start()
         connection.send(f"Welcome to the server {address[0]}! \n".encode(FORMAT))
@@ -42,6 +80,5 @@ def start_server():
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
         
         
-
 print("[STARTING] server is starting...")
 start_server()
